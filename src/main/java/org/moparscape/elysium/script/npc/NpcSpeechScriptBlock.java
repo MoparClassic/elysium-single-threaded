04af82ed4a1e4f5cdb0e8e1df46f5c7cafc82745
DanLoth <lothyis@gmail.com>
2015-02-13 10:48:25 +1100

    Removed NPC conversation scripting stuff.

--- src/main/java/org/moparscape/elysium/script/npc/NpcSpeechScriptBlock.java
+++
@@ -1,7 +0,0 @@
-package org.moparscape.elysium.script.npc;
-
-/**
- * Created by daniel on 11/02/2015.
- */
-public class NpcSpeechScriptBlock {
-}
--- src/main/java/org/moparscape/elysium/script/npc/ScriptBlock.java
+++
@@ -1,9 +0,0 @@
-package org.moparscape.elysium.script.npc;
-
-/**
- * Created by daniel on 2/02/2015.
- */
-public interface ScriptBlock {
-
-    void run(NpcTalkScriptContext context);
-}
